a3b6ea06381660aa48108d6e24ccb07c767c4c9f
MaratShayafetdinov <70947396+MaratShayafetdinov@users.noreply.github.com>
2020-09-21 22:57:30 +0300

    Practica1.1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practica1
+{
+    struct Person
+    {
+        /// <summary>
+        /// поля
+        /// </summary>
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+       
+        public Person(string _fistName, string _lastName, int _age)
+        {
+            firstName = _fistName;
+            lastName = _lastName;
+            age = _age;
+        }
+
+        /// <summary>
+        /// переопределение метода ToString
+        /// </summary>
+        
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + ", age " + age; 
+        }
+
+
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
